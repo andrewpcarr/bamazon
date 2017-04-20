80fe3d87da1095957e99ecf8306afdd50b435e34
Andrew Carr <andrewpcarr@gmail.com>
2017-04-19 19:16:30 -0500

    cleaned up commented out code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,7 +9,7 @@ var connection = mysql.createConnection({
   user: "root",
 
   // Your password
-  password: "BaQaR049!",
+  password: "",
   database: "bamazon"
 });
 
@@ -58,79 +58,3 @@ var buyProduct = function() {
     });
   });
 };
-
-// var quantity = function() {
-//   inquirer.prompt({
-//     name: "product",
-//     type: "input",
-//     message: "How many would you like to purchase?"
-//   }).then(function(answer) {
-//     var query = "SELECT id, product_name, price FROM products WHERE id = " + answer.product;
-//     connection.query(query, function(err, res) {
-    
-    
-//     for (i = 0; i < res.length; i++) {
-//         console.log("You've selected: " + res[i].product_name);
-//     }
-//     quantity();
-//     });
-//   });
-// };
-
-// var multiSearch = function() {
-//   var query = "SELECT artist FROM top5000 GROUP BY artist HAVING count(*) > 1";
-//   connection.query(query, function(err, res) {
-//     for (var i = 0; i < res.length; i++) {
-//       console.log(res[i].artist);
-//     }
-//     runSearch();
-//   });
-// };
-
-// var rangeSearch = function() {
-//   inquirer.prompt([{
-//     name: "start",
-//     type: "input",
-//     message: "Enter starting position: ",
-//     validate: function(value) {
-//       if (isNaN(value) === false) {
-//         return true;
-//       }
-//       return false;
-//     }
-//   }, {
-//     name: "end",
-//     type: "input",
-//     message: "Enter ending position: ",
-//     validate: function(value) {
-//       if (isNaN(value) === false) {
-//         return true;
-//       }
-//       return false;
-//     }
-//   }]).then(function(answer) {
-//     var query = "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
-//     connection.query(query, [answer.start, answer.end], function(err, res) {
-//       for (var i = 0; i < res.length; i++) {
-//         console.log("Position: " + res[i].position + " || Song: " + res[i].song
-//           + " || Artist: " + res[i].artist + " || Year: " + res[i].year);
-//       }
-//       runSearch();
-//     });
-//   });
-// };
-
-// var songSearch = function() {
-//   inquirer.prompt({
-//     name: "song",
-//     type: "input",
-//     message: "What song would you like to look for?"
-//   }).then(function(answer) {
-//     console.log(answer.song);
-//     connection.query("SELECT * FROM top5000 WHERE ?", { song: answer.song }, function(err, res) {
-//       console.log("Position: " + res[0].position + " || Song: " + res[0].song
-//         + " || Artist: " + res[0].artist + " || Year: " + res[0].year);
-//       runSearch();
-//     });
-//   });
-// };
